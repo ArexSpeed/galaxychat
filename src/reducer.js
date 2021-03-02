a264d26c3ef8a6434fb3aa6d7d34d0e1991c6517
Arek Cichocki <arexspeed95@gmail.com>
2021-03-02 15:01:27 +0100

    store reducer

---
+++ src/reducer.js
@@ -0,0 +1,43 @@
+export const initialState = {
+  user: null,
+  active: false,
+  color: "blue",
+  usersList: []
+};
+
+export const actionTypes = {
+  SET_USER: "SET_USER",
+  SET_ACTIVE: "SET_ACTIVE",
+  SET_COLOR: "SET_COLOR",
+  SET_USERS_LIST: "SET_USERS_LIST"
+};
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case actionTypes.SET_USER:
+      return {
+        ...state,
+        user: action.payload
+      };
+    case actionTypes.SET_ACTIVE:
+      return {
+        ...state,
+        active: action.payload
+      };
+    case actionTypes.SET_COLOR:
+      return {
+        ...state,
+        color: action.payload
+      };
+    case actionTypes.SET_USERS_LIST:
+        return {
+          ...state,
+          usersList: action.payload
+        };
+
+    default:
+      return state;
+  }
+};
+
+export default reducer;
